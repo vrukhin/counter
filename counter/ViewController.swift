4e5ff2e4ce56fe2455c736beda8c26bc5e5f27fd
Pavel Vrukhin <vrukhin@protonmail.com>
2023-03-19 14:01:35 +0300

    Решена усложненная задача     * Добавлены кнопки уменьшения и сброса значения счетчика     * Добавлен запрет на уменьшение счетчика ниже 0     * Добавлена история изменений

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counterAddButton: UIButton!
+    @IBOutlet weak var counterSubtractButton: UIButton!
     @IBOutlet weak var counterValueLabel: UILabel!
-    private var counterValue: Int = 0
+    @IBOutlet weak var counterResetButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var applicationNameLabel: UILabel!
+    var counterValue: Int = 0
+
+    let applicationName = "Счетчик"
+
+    func getCurrentDatetime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd hh:mm:ss"
+        return dateFormatter.string(from: Date.init())
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        applicationNameLabel.text = applicationName
         counterValueLabel.text = String(counterValue)
+        historyTextView.text = ""
+        historyTextView.isEditable = false
     }
 
     @IBAction func counterAddButtonDidTap(_ sender: Any) {
         counterValue += 1
         counterValueLabel.text = String(counterValue)
+        let datetime = getCurrentDatetime()
+        historyTextView.text += "\(datetime)\nзначение изменено на +1\n"
+    }
+    
+    @IBAction func counterSubtractButtonDidTap(_ sender: Any) {
+        let datetime = getCurrentDatetime()
+        if counterValue > 0 {
+            counterValue -= 1
+            historyTextView.text += "\(datetime)\nзначение изменено на -1\n"
+        } else {
+            counterValue = 0
+            historyTextView.text += "\(datetime)\nпопытка уменьшить значение счетчика ниже нуля\n"
+        }
+        counterValueLabel.text = String(counterValue)
+        
+        
+    }
+    
+    @IBAction func counterResetButtonDidTap(_ sender: Any) {
+        counterValue = 0
+        counterValueLabel.text = String(counterValue)
+        let datetime = getCurrentDatetime()
+        historyTextView.text += "\(datetime)\nзначение сброшено\n"
     }
 }
 
